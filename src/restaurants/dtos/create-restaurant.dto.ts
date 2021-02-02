cf65e5c1f723c03e742b1bdfb9a7ef90e2211678
Kyungmi Koong <kyungmi.k@woowahan.com>
2021-02-02 09:10:41 +0900

    #1.5 Validating ArgTypes

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,9 +1,22 @@
 import { ArgsType, Field, InputType } from "@nestjs/graphql";
+import { IsBoolean, IsString, Length } from "class-validator";
 
 @ArgsType()
 export class createRestaurantDto {
-    @Field(type => String) name: string;
-    @Field(type => Boolean) isVegan: boolean;
-    @Field(type => String) address: string;
-    @Field(type => String) ownerName: string;
+    @Field(type => String)
+    @IsString()
+    @Length(5, 10)
+    name: string;
+
+    @Field(type => Boolean)
+    @IsBoolean()
+    isVegan: boolean;
+
+    @Field(type => String)
+    @IsString()
+    address: string;
+
+    @Field(type => String)
+    @IsString()
+    ownerName: string;
 }
--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,15 +1,19 @@
 // 데이터베이스의 모델
 
 import { Field, ObjectType } from "@nestjs/graphql";
+import { IsBoolean, IsString, Length } from "class-validator";
 
 @ObjectType()
 export class Restaurant {
     @Field(type => String)
     name: string;
+
     @Field(type => Boolean)
     isVegan: boolean;
+
     @Field(type => String)
     address: string;
+
     @Field(type => String)
     ownerName: string;
 }
