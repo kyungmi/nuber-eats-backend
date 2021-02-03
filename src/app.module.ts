93427cef4e7dd06ce24302ff2cbf5e9bc31954c3
Kyungmi Koong <kyungmi.k@woowahan.com>
2021-02-03 21:28:49 +0900

    #2.5 Configuring ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';  // Apollo Server로 구현되어 있음
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.ENV}`,
+      ignoreEnvFile: process.env.ENV === 'prod',
+    }),
     GraphQLModule.forRoot({
       // Apollo Server 설정
       // Code first - *.graphql 파일을 만들지 않고, decorator와 Typescript class를 사용해서 GraphQL schema를 만든다.
@@ -13,11 +19,11 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
     }),
     TypeOrmModule.forRoot({
       "type": "postgres",
-      "host": "localhost",
-      "port": 5432,
-      "username": "gunggyeongmi",
-      "password": "12345",
-      "database": "nuber-eats",
+      "host": process.env.DB_HOST,
+      "port": +process.env.DB_PORT,
+      "username": process.env.DB_USERNAME,
+      "password": process.env.DB_PASSWORD,
+      "database": process.env.DB_DATABASE,
       "synchronize": true,
       "logging": true,
     }),
