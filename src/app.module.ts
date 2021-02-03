2277fdd5ece3e7cb38596b42e6720f6acdac102d
Kyungmi Koong <kyungmi.k@woowahan.com>
2021-02-03 21:07:27 +0900

    #2.3 TypeORM Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';  // Apollo Server로 구현되어 있음
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
@@ -9,6 +10,16 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       // Code first - *.graphql 파일을 만들지 않고, decorator와 Typescript class를 사용해서 GraphQL schema를 만든다.
       // autoSchemaFile을 메모리에 보관하고 싶다면 true로 한다.
       autoSchemaFile: true // join(process.cwd(), 'src/schema.gql'),
+    }),
+    TypeOrmModule.forRoot({
+      "type": "postgres",
+      "host": "localhost",
+      "port": 5432,
+      "username": "gunggyeongmi",
+      "password": "12345",
+      "database": "nuber-eats",
+      "synchronize": true,
+      "logging": true,
     }),
     RestaurantsModule
   ],
