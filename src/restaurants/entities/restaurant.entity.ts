1179996ac605ed1874dfe310c08a877de821a975
Kyungmi Koong <kyungmi.k@woowahan.com>
2021-02-04 21:42:26 +0900

    #3.6 Optional Types and Columns

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,7 +1,13 @@
 // 데이터베이스의 모델
 
 import { Field, Int, ObjectType } from '@nestjs/graphql';
-import { IsBoolean, IsString, Length, MinLength } from 'class-validator';
+import {
+  IsBoolean,
+  IsOptional,
+  IsString,
+  Length,
+  MinLength,
+} from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
@@ -11,18 +17,19 @@ export class Restaurant {
   @Field((type) => Int)
   id: number;
 
-  @Field((type) => String)
-  @Column()
-  @IsString()
-  @MinLength(5)
+  @Field((type) => String) // for graphql
+  @Column() // for database
+  @IsString() // for DTO's validation
+  @MinLength(5) // for DTO's validation
   name: string;
 
-  @Field((type) => Boolean)
-  @Column()
+  @Field((type) => Boolean, { nullable: true }) // defaultValue: true로 설정하면 dto에 누락된 내용을 채워서 graphql 요청, nullable: true인 경우 누락된 내용을 보내지 않음
+  @Column({ default: true }) // DB의 DDL에 디폴트 값 설정
+  @IsOptional()
   @IsBoolean()
   isVegan: boolean;
 
-  @Field((type) => String)
+  @Field((type) => String, { defaultValue: 'abcdefg' })
   @Column()
   @IsString()
   address: string;
