af1fe9b671ffe402c00ee349e0e022e4f06f702a
Kyungmi Koong <kyungmi.k@woowahan.com>
2021-02-08 11:44:44 +0900

    #5.4 JWT Module part Two

--- src/jwt/jwt.service.ts
+++ src/jwt/jwt.service.ts
@@ -1,4 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { JwtModuleOptions } from './jwt.interfaces';
+import { CONFIG_OPTIONS } from './jwt.constant';
 
 @Injectable()
-export class JwtService {}
+export class JwtService {
+  constructor(
+    @Inject(CONFIG_OPTIONS) private readonly options: JwtModuleOptions,
+  ) {}
+}
