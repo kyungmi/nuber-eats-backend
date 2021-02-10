b2c4b67bb6ab74adda7bdf5e629118791b737743
Kyungmi Koong <kyungmi.k@woowahan.com>
2021-02-10 18:54:10 +0900

    #5.7 JWT Middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -56,4 +56,10 @@ import { JwtMiddleware } from './jwt/jwt.middleware';
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(JwtMiddleware)
+      .forRoutes({ path: '/graphql', method: RequestMethod.ALL });
+  }
+}
