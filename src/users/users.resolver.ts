d708444c1746331d3d82797a45b17398acdb9729
Kyungmi Koong <kyungmi.k@woowahan.com>
2021-02-11 22:45:10 +0900

    #5.12 userProfile Mutation

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -7,6 +7,7 @@ import {
   CreateAccountOutput,
 } from './dtos/create-account.dto';
 import { LoginInput, LoginOutput } from './dtos/login.dto';
+import { UserProfileInput, UserProfileOutput } from './dtos/user-profile.dto';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
@@ -38,5 +39,22 @@ export class UsersResolver {
   @UseGuards(AuthGuard)
   me(@AuthUser() authUser: User) {
     return authUser;
+  }
+
+  @UseGuards(AuthGuard)
+  @Query((returns) => UserProfileOutput)
+  async userProfile(
+    @Args() userProfileInput: UserProfileInput,
+  ): Promise<UserProfileOutput> {
+    try {
+      const user = await this.usersService.findById(userProfileInput.userId);
+      if (user) {
+        return { ok: true, user };
+      } else {
+        return { ok: false, error: 'User Not Found' };
+      }
+    } catch (error) {
+      return { ok: false, error: 'User Not Found' };
+    }
   }
 }
