acb59649e8e00a16188cb6ebb99e79d1c18f25f5
Kyungmi Koong <kyungmi.k@woowahan.com>
2021-02-12 12:49:34 +0900

    #6.2 Verifying User part One

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -78,4 +78,16 @@ export class UsersService {
     }
     return this.users.save(user);
   }
+
+  async verifyEmail(code: string): Promise<boolean> {
+    const verification = await this.verifications.findOne(
+      { code },
+      { relations: ['user'] },
+    );
+    if (verification) {
+      verification.user.verified = true;
+      this.users.save(verification.user);
+    }
+    return false;
+  }
 }
