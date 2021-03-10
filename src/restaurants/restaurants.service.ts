d6ca6540b36055b352aeb12cad0ebfe4eac7033e
Kyungmi Koong <kyungmi.k@woowahan.com>
2021-03-10 23:40:58 +0900

    10.18 Search part Two

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
-import { Like, Repository } from 'typeorm';
+import { Like, Raw, Repository } from 'typeorm';
 import { AllCategoriesOutput } from './dtos/all-categories.dto';
 import { CategoryInput, categoryOutput } from './dtos/category.dto';
 import {
@@ -216,7 +216,7 @@ export class RestaurantService {
   }: SearchRestaurantInput): Promise<SearchRestaurantOutput> {
     try {
       const [restaurants, totalResults] = await this.restaurants.findAndCount({
-        where: { name: Like(`%${query}%`) },
+        where: { name: Raw((name) => `${name} ILIKE '%${query}%'`) },
         skip: (page - 1) * 25,
         take: 25,
       });
