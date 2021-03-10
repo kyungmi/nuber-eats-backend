3b3bb90483bb175c1e3ea1dacc5e444288ef2cae
Kyungmi Koong <kyungmi.k@woowahan.com>
2021-03-10 23:21:36 +0900

    10.16 Restaurants

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -16,6 +16,7 @@ import {
   EditRestaurantInput,
   EditRestaurantOutput,
 } from './dtos/edit-restaurant.dto';
+import { RestaurantsInput, RestaurantsOutput } from './dtos/restaurants.dto';
 import { Category } from './entities/category.entity';
 import { Restaurant } from './entities/restaurant.entity';
 import { CategoryRepository } from './repositories/category.repository';
@@ -143,12 +144,13 @@ export class RestaurantService {
         take: 25,
         skip: (page - 1) * 25,
       });
-      category.restaurants = restaurants;
       const totalResults = await this.countRestaurant(category);
       return {
         ok: true,
         category,
+        restaurants,
         totalPages: Math.ceil(totalResults / 25),
+        totalResults,
       };
     } catch (e) {
       return {
@@ -160,5 +162,25 @@ export class RestaurantService {
 
   async countRestaurant(category: Category) {
     return this.restaurants.count({ category });
+  }
+
+  async allRestaurants({ page }: RestaurantsInput): Promise<RestaurantsOutput> {
+    try {
+      const [restaurants, totalResults] = await this.restaurants.findAndCount({
+        skip: (page - 1) * 25,
+        take: 25,
+      });
+      return {
+        ok: true,
+        results: restaurants,
+        totalPages: Math.ceil(totalResults / 25),
+        totalResults,
+      };
+    } catch (e) {
+      return {
+        ok: false,
+        error: 'Could not load restaurants',
+      };
+    }
   }
 }
