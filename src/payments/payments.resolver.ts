c3f2fb295f4db8b3d21af9823f29b2a9ef747458
Kyungmi Koong <kyungmi.k@woowahan.com>
2021-04-03 12:04:59 +0900

    13.3 createPayment part Two, 13.4 getPayments Resolver

--- src/payments/payments.resolver.ts
+++ src/payments/payments.resolver.ts
@@ -1,8 +1,12 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver, Query } from '@nestjs/graphql';
+import { AuthUser } from 'src/auth/auth-user.decorator';
+import { Role } from 'src/auth/role.decorator';
+import { User, UserRole } from 'src/users/entities/user.entity';
 import {
   CreatePaymentInput,
   CreatePaymentOutput,
 } from './dtos/create-payment.dto';
+import { GetPaymentsOutput } from './dtos/get-payments.dto';
 import { Payment } from './entities/payment.entity';
 import { PaymentsService } from './payments.service';
 
@@ -11,7 +15,17 @@ export class PaymentsResolver {
   constructor(private readonly paymentService: PaymentsService) {}
 
   @Mutation((returns) => CreatePaymentOutput)
+  @Role([UserRole.Owner])
   createPayment(
+    @AuthUser() owner: User,
     @Args('input') createPaymentInput: CreatePaymentInput,
-  ): Promise<CreatePaymentOutput> {}
+  ): Promise<CreatePaymentOutput> {
+    return this.paymentService.createPayment(owner, createPaymentInput);
+  }
+
+  @Query((returns) => GetPaymentsOutput)
+  @Role([UserRole.Owner])
+  getPayments(@AuthUser() user: User): Promise<GetPaymentsOutput> {
+    return this.paymentService.getPayments(user);
+  }
 }
